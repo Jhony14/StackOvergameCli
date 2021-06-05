2e2258cb57fd5c1f5d43c23a2094f5c8b1e213a6
Jhonatan <jhonyart14@gmail.com>
2021-06-05 10:28:44 +0200

    Correct profile image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,26 @@ export class AppComponent {
 
   constructor(public service: SharedService) { }
 
+  public imageProfile: string;
+
   ngOnInit(): void {
     console.log(this.service.getToken() + " token")
     if (this.service.getToken()) {
       this.service.updateData(this.service.getToken())
     }
+
+    this.getProfileImage();
   }
 
+
+  getProfileImage() {
+    this.service.getUsuario(this.service.userId).subscribe(data => {
+      console.log("%% USER IMAGE ", data[0].Imagenperfil);
+      this.imageProfile = data[0].Imagenperfil;
+    }, error => {
+      console.log("ERROR IMAGE USER ", error);
+    });
+  }
 
 
 }
